ff5a75b1ef786540e23a22974144ab1508ace95d
Disha Khakhar <dikhakha@microsoft.com>
2018-11-12 15:56:38 +0530

    Actions for Azure cli, login and template deployment

---
+++ azure-template-deployment/entrypoint.sh
@@ -0,0 +1,10 @@
+az group create --name "$RESOURCE_GROUP_NAME" --location "$RESOURCE_GROUP_LOCATION"
+
+if [ -n "$TEMPLATE_FILE_PATH" ]
+then
+  COMMAND="group deployment create -g \"$RESOURCE_GROUP_NAME\" --name \"$DEPLOYMENT_NAME\" --template-file \"$GITHUB_WORKSPACE/$TEMPLATE_FILE_PATH\" --parameters \"@$GITHUB_WORKSPACE/$PARAMETERS_FILE_PATH\""
+else
+  COMMAND="group deployment create -g \"$RESOURCE_GROUP_NAME\" --name \"$DEPLOYMENT_NAME\" --template-uri "$TEMPLATE_URI" --parameters \"@$GITHUB_WORKSPACE/$PARAMETERS_FILE_PATH\""
+fi
+
+az $COMMAND
