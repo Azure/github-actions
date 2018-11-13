3f4452e13a32b52b8d3c8ea7b5328f9c1b2561de
Disha Khakhar <dikhakha@microsoft.com>
2018-11-13 13:33:59 +0530

    condition check

--- azure-template-deployment/entrypoint.sh
+++ azure-template-deployment/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 az group create --name "$RESOURCE_GROUP_NAME" --location "$RESOURCE_GROUP_LOCATION"
 
-if [ -n "${TEMPLATE_FILE_PATH}" ]
+if [ -z "${TEMPLATE_FILE_PATH}" ]
 then
-  az group deployment create -g "${RESOURCE_GROUP_NAME}" --name "${DEPLOYMENT_NAME}" --template-file "${GITHUB_WORKSPACE}/${TEMPLATE_FILE_PATH}" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
-else
   az group deployment create -g "${RESOURCE_GROUP_NAME}" --name "${DEPLOYMENT_NAME}" --template-uri "$TEMPLATE_URI" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
+else
+  az group deployment create -g "${RESOURCE_GROUP_NAME}" --name "${DEPLOYMENT_NAME}" --template-file "${GITHUB_WORKSPACE}/${TEMPLATE_FILE_PATH}" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
 fi
