bc9dbaff7aaba66bf8cc42b1b4c85cafebbf895c
Disha Khakhar <dikhakha@microsoft.com>
2018-11-13 14:15:55 +0530

    regex

--- azure-template-deployment/entrypoint.sh
+++ azure-template-deployment/entrypoint.sh
@@ -4,9 +4,7 @@ set -e
 
 az group create --name "$RESOURCE_GROUP_NAME" --location "$RESOURCE_GROUP_LOCATION"
 
-URI_REGEX="^(http://|https://)\\w+"
-
-if [[ ${TEMPLATE_FILE} =~ $URI_REGEX ]]; then
+if [[ ${TEMPLATE_FILE} =~ ^(http://|https://)\\w+ ]]; then
   az group deployment create -g "${RESOURCE_GROUP_NAME}" --name "${DEPLOYMENT_NAME}" --template-uri "$TEMPLATE_FILE" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
 else
   az group deployment create -g "${RESOURCE_GROUP_NAME}" --name "${DEPLOYMENT_NAME}" --template-file "${GITHUB_WORKSPACE}/${TEMPLATE_FILE}" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
