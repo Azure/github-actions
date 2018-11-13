49a6af1ceb7cfa70d1df197b62933f006ed628fe
nibansal <nibansal@microsoft.com>
2018-11-13 10:08:30 +0530

    echo cmd removed

--- az-pipelines/entrypoint.sh
+++ az-pipelines/entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 if [ -n "$AZURE_PIPELINE_ORGANIZATION" ] && [ -n "$AZURE_PIPELINE_PROJECT" ] && [ -n "$AZURE_PIPELINE_TOKEN" ] && [ -n "$AZURE_PIPELINE_NAME" ]; then
     
     AZDEVOPS_URL="https://dev.azure.com/${AZURE_PIPELINE_ORGANIZATION}/"
-    echo "Configuring Azure pipeline URL: "$AZDEVOPS_URL
     vsts configure --defaults instance=${AZDEVOPS_URL} project=${AZURE_PIPELINE_PROJECT}
     
     vsts login --token ${AZURE_PIPELINE_TOKEN}
