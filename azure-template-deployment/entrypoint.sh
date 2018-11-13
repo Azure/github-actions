47288db3fd009f4174ca490969b826d61d9f5e15
Disha Khakhar <dikhakha@microsoft.com>
2018-11-13 12:04:01 +0530

    update

--- azure-template-deployment/entrypoint.sh
+++ azure-template-deployment/entrypoint.sh
@@ -6,7 +6,7 @@ az group create --name "$RESOURCE_GROUP_NAME" --location "$RESOURCE_GROUP_LOCATI
 
 if [ -v "${TEMPLATE_FILE_PATH}" -a -n "${TEMPLATE_FILE_PATH}" ]
 then
-  az group deployment create -g "${RESOURCE_GROUP_NAME}" --name "${DEPLOYMENT_NAME}" --template-uri "$TEMPLATE_URI" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
-else
   az group deployment create -g "${RESOURCE_GROUP_NAME}" --name "${DEPLOYMENT_NAME}" --template-file "${GITHUB_WORKSPACE}/${TEMPLATE_FILE_PATH}" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
+else
+  az group deployment create -g "${RESOURCE_GROUP_NAME}" --name "${DEPLOYMENT_NAME}" --template-uri "$TEMPLATE_URI" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
 fi
