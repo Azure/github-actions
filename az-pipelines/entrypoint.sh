5045193af3c81aad4f262837f5ac77ff6c3652c7
nibansal <nibansal@microsoft.com>
2018-11-13 10:04:45 +0530

    review changes done

--- az-pipelines/entrypoint.sh
+++ az-pipelines/entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e 
 
-if [ -n "$AZURE_PIPELINE_ORGANIZATION" ] && [ -n "$AZURE_PIPELINE_PROJECT" ]; then
+if [ -n "$AZURE_PIPELINE_ORGANIZATION" ] && [ -n "$AZURE_PIPELINE_PROJECT" ] && [ -n "$AZURE_PIPELINE_TOKEN" ] && [ -n "$AZURE_PIPELINE_NAME" ]; then
+    
     AZDEVOPS_URL="https://dev.azure.com/${AZURE_PIPELINE_ORGANIZATION}/"
-	echo "Configuring Azure pipeline URL: "$AZDEVOPS_URL
+    echo "Configuring Azure pipeline URL: "$AZDEVOPS_URL
     vsts configure --defaults instance=${AZDEVOPS_URL} project=${AZURE_PIPELINE_PROJECT}
-
-	if [ -n "$AZURE_PIPELINE_TOKEN" ]; then
-	    vsts login --token ${AZURE_PIPELINE_TOKEN}
-		echo "Queueing Azure build pipeline for Pipeline : ${AZURE_PIPELINE_NAME}"
-        vsts build queue --definition-name ${AZURE_PIPELINE_NAME}
-	fi
+    
+    vsts login --token ${AZURE_PIPELINE_TOKEN}
+    
+    echo "Queueing Azure pipeline: ${AZURE_PIPELINE_NAME}"
+    vsts build queue --definition-name ${AZURE_PIPELINE_NAME}
 fi
