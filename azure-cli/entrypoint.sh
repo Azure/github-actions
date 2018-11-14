7a88b17c01092a2aa9cf9d2137c4210846885662
Disha Khakhar <dikhakha@microsoft.com>
2018-11-14 12:34:57 +0530

    script path

--- azure-cli/entrypoint.sh
+++ azure-cli/entrypoint.sh
@@ -2,4 +2,13 @@
 
 set -e
 
-sh -c "$*"
+declare AZ_SCRIPT
+
+if [ -n "$SCRIPT_PATH" ]
+then
+  AZ_SCRIPT = "${GITHUB_WORKSPACE}/${SCRIPT_PATH}"
+else
+  AZ_SCRIPT = "$*"
+fi
+
+sh -c "${AZ_SCRIPT}"
