d5f8a31bc5f604b33ee55e76518216dc61326e9a
Disha Khakhar <dikhakha@microsoft.com>
2018-11-14 14:17:33 +0530

    final

--- azure-cli/entrypoint.sh
+++ azure-cli/entrypoint.sh
@@ -4,9 +4,8 @@ set -e
 
 if [ -n "$SCRIPT_PATH" ]
 then
-  chmod +x ${GITHUB_WORKSPACE}/$SCRIPT_PATH
-  ${GITHUB_WORKSPACE}/$SCRIPT_PATH
+  chmod +x ${GITHUB_WORKSPACE}/${SCRIPT_PATH}
+  ${GITHUB_WORKSPACE}/${SCRIPT_PATH}
 else
-  echo "Executing command $*"
   sh -c "$*"
 fi
