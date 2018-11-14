89a889452d4c13e114943298ae8ab63adf389248
Disha Khakhar <dikhakha@microsoft.com>
2018-11-14 12:24:56 +0530

    az cli line endings

--- azure-cli/entrypoint.sh
+++ azure-cli/entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sh -c "az $*"
+sh -c "$*"
--- azure-template-deployment/entrypoint.sh
+++ azure-template-deployment/entrypoint.sh
@@ -8,7 +8,7 @@ URI_REGEX="^(http://|https://)\\w+"
 
 if [[ $TEMPLATE_FILE =~ $URI_REGEX ]]
 then
-  az group deployment create -g "$RESOURCE_GROUP_NAME" --name "$DEPLOYMENT_NAME" --template-uri "$TEMPLATE_FILE" --parameters "@$GITHUB_WORKSPACE/$PARAMETERS_FILE_PATH"
+  az group deployment create -g "$RESOURCE_GROUP_NAME" --name "$DEPLOYMENT_NAME" --template-uri "$TEMPLATE_FILE" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
 else
-  az group deployment create -g "$RESOURCE_GROUP_NAME" --name "$DEPLOYMENT_NAME" --template-file "$GITHUB_WORKSPACE/$TEMPLATE_FILE" --parameters "@$GITHUB_WORKSPACE/$PARAMETERS_FILE_PATH"
+  az group deployment create -g "$RESOURCE_GROUP_NAME" --name "$DEPLOYMENT_NAME" --template-file "${GITHUB_WORKSPACE}/${TEMPLATE_FILE}" --parameters "@${GITHUB_WORKSPACE}/${PARAMETERS_FILE_PATH}"
 fi
