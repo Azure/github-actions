8398768329ffa701d613ccca47d61f9827e555bd
Deepak Sattiraju <desattir@microsoft.com>
2018-11-14 15:49:49 +0530

    Adding default helm chart

--- aks-deploy/entrypoint.sh
+++ aks-deploy/entrypoint.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# Respect AZ_OUTPUT_FORMAT if specified
-[ -n "$AZ_OUTPUT_FORMAT" ] || export AZ_OUTPUT_FORMAT=json
-
 if [ -n "$AZURE_SERVICE_PEM" ]; then
   mkdir -p "$HOME/.az"
   echo "$AZURE_SERVICE_PEM" > "$HOME/.az/key.pem"
@@ -19,16 +16,24 @@ az aks get-credentials --name "$AKS_CLUSTER_NAME" --resource-group "$RESOURCE_GR
 
 if [ -n "$DOCKER_REGISTRY_URL" ] && [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
   echo "Adding docker secrets"
-  kubectl create secret docker-registry dockerPullSecret --docker-server=$DOCKER_REGISTRY_URL --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD
+  if [ -n "$DOCKER_EMAIL" ]; then
+    kubectl create secret docker-registry docker-pull-secret --docker-server=$DOCKER_REGISTRY_URL --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL --dry-run -o json | kubectl apply -f -
+  else
+    kubectl create secret docker-registry docker-pull-secret --docker-server=$DOCKER_REGISTRY_URL --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=default@example.com --dry-run -o json | kubectl apply -f -
+  fi
 fi
 
 helm init
 
-if [ -n "$HELM_CHART_PATH" ]; then
-  helm upgrade --install --force $* aksdeploy $HELM_CHART_PATH   
-else
-  echo "Creating a helm chart"
-  helm create aksdeploy
-  HELM_CHART_PATH=./aksdeploy
-  helm upgrade --install --force --set image.repository=$IMAGE_NAME --set image.tag=latest --set service.type=LoadBalancer aksdeploy $HELM_CHART_PATH 
+DEFAULT_ARGS=""
+
+if [ -z "$HELM_CHART_PATH" ]; then
+  echo "Using a default helm chart"
+  HELM_CHART_PATH=/default-chart
+  if [ -n "$IMAGE_TAG" ]; then
+    IMAGE_NAME=$IMAGE_NAME:$IMAGE_TAG
+  fi
+  DEFAULT_ARGS="--set image.repository=$IMAGE_NAME"
 fi
+
+helm upgrade --install --force $DEFAULT_ARGS $* aksdeploy $HELM_CHART_PATH
