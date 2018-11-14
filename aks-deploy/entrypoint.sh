689962ef12d27792fece031be9d3201474ade7db
Deepak Sattiraju <desattir@microsoft.com>
2018-11-14 17:16:12 +0530

    Making RG_Name optional

--- aks-deploy/entrypoint.sh
+++ aks-deploy/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "$AKS_CLUSTER_NAME" ]; then
+    echo "\$AKS_CLUSTER_NAME is not set."
+    exit 1
+fi
+
 if [ -n "$AZURE_SERVICE_PEM" ]; then
   mkdir -p "$HOME/.az"
   echo "$AZURE_SERVICE_PEM" > "$HOME/.az/key.pem"
@@ -12,7 +17,18 @@ if [ -n "$AZURE_SERVICE_APP_ID" ] && [ -n "$AZURE_SERVICE_PASSWORD" ] && [ -n "$
   az login --service-principal --username "$AZURE_SERVICE_APP_ID" --password "$AZURE_SERVICE_PASSWORD" --tenant "$AZURE_SERVICE_TENANT"
 fi
 
-az aks get-credentials --name "$AKS_CLUSTER_NAME" --resource-group "$RESOURCE_GROUP"
+if [ -z "$RESOURCE_GROUP" ]; then
+  AMBIGUOUS=$(az resource list --name $AKS_CLUSTER_NAME --resource-type Microsoft.ContainerService/managedClusters --query "[1].resourceGroup")
+  if [ -n "$AMBIGUOUS" ]; then
+    echo "Provided AKS cluster name is ambiguous, provide \$RESOURCE_GROUP to identify the cluster correctly"
+    exit 1
+  else
+    RESOURCE_GROUP=$(az resource list --name $AKS_CLUSTER_NAME --resource-type Microsoft.ContainerService/managedClusters --query "[0].resourceGroup" -o tsv)
+    echo "Recognized RG name: $RESOURCE_GROUP"
+  fi
+fi
+
+az aks get-credentials --name $AKS_CLUSTER_NAME --resource-group $RESOURCE_GROUP
 
 if [ -n "$DOCKER_REGISTRY_URL" ] && [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
   echo "Adding docker secrets"
@@ -29,6 +45,12 @@ DEFAULT_ARGS=""
 
 if [ -z "$HELM_CHART_PATH" ]; then
   echo "Using a default helm chart"
+
+  if [ -z "$IMAGE_NAME" ]; then
+      echo "\$IMAGE_NAME is not set."
+      exit 1
+  fi
+
   HELM_CHART_PATH=/default-chart
   if [ -n "$IMAGE_TAG" ]; then
     IMAGE_NAME=$IMAGE_NAME:$IMAGE_TAG
