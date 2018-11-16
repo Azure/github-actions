c8a0e7c977b03ae889e67e142495d2a22d5939b5
vinca <vinca@microsoft.com>
2018-11-16 23:18:17 +0530

    updated function app

--- function-app-deploy/entrypoint.sh
+++ function-app-deploy/entrypoint.sh
@@ -11,14 +11,11 @@ then
 fi
 echo "Web App Name: ${WEB_APP_NAME}"
 
+RESOURCE_GROUP_NAME=`az resource list -n "${WEB_APP_NAME}" --resource-type "Microsoft.Web/Sites" --query '[0].resourceGroup' | xargs`
 if [[ -z $RESOURCE_GROUP_NAME ]];
-then 
-    RESOURCE_GROUP_NAME=`az resource list -n "${WEB_APP_NAME}" --resource-type "Microsoft.Web/Sites" --query '[0].resourceGroup' | xargs`
-    if [[ -z $RESOURCE_GROUP_NAME ]];
-    then
-        echo "Web App '${WEB_APP_NAME}' should exist before deployment." >&2
-        exit 1
-    fi
+then
+    echo "Web App '${WEB_APP_NAME}' should exist before deployment." >&2
+    exit 1
 fi
 
 echo "Resource Group Name: ${RESOURCE_GROUP_NAME}"
@@ -53,14 +50,17 @@ then
 fi
 
 echo "Retrieving publishing credentials for the app"
-DEPLOYUSER=`az webapp deployment list-publishing-profiles -n ${WEB_APP_NAME} -g ${RESOURCE_GROUP_NAME} --query '[0].userName' -o tsv`
-DEPLOYPASS=`az webapp deployment list-publishing-profiles -n ${WEB_APP_NAME} -g ${RESOURCE_GROUP_NAME} --query '[0].userPWD' -o tsv`
+PUBLISH_PROFILE=`az webapp deployment list-publishing-profiles -n ${WEB_APP_NAME} -g ${RESOURCE_GROUP_NAME}`
+
+
+DEPLOYUSER=`node -pe 'JSON.parse(process.argv[1])[0].userName' ${PUBLISH_PROFILE}`
+DEPLOYPASS=`node -pe 'JSON.parse(process.argv[1])[0].userPWD' ${PUBLISH_PROFILE}`
 
 echo "Initiating Zip Deploy"
-node /main.js --action zipdeploy --scmUri "https://${WEB_APP_NAME}.scm.azurewebsites.net" --username $DEPLOYUSER --password $DEPLOYPASS --package "$PACKAGE_PATH"
-echo "Package Deployed to Azure Function App."
+node /node_modules/typed-azure-client/runner/kuduService.js --action zipdeploy --scmUri "https://${WEB_APP_NAME}.scm.azurewebsites.net" --username $DEPLOYUSER --password $DEPLOYPASS --package "$PACKAGE_PATH"
+echo "Package Deployed to Azure Web App."
 
-DESTINATION_URL=`az webapp deployment list-publishing-profiles -n ${WEB_APP_NAME} -g ${RESOURCE_GROUP_NAME} --query '[0].destinationAppUrl' -o tsv`
+DESTINATION_URL=`node -pe 'JSON.parse(process.argv[1])[0].destinationAppUrl' ${PUBLISH_PROFILE}`
 echo "App Service Application URL: ${DESTINATION_URL}"
 
 
