319504480c7ca44b4ae46980988f2335e60912fe
Deepak Sattiraju <desattir@microsoft.com>
2018-11-19 17:14:39 +0530

    [aks-deploy] Adding default helm chart (#7)
    * Adding default helm chart
    
    * Adding image pull secret
    
    * Making RG_Name optional
    
    * Binding resource names to release.name
    
    * Check if cluster exists
    
    * Fixing check
    
    * Removing self update var
    
    * Fixing bad substitution
    
    * Updating variable names to match spec

--- aks-deploy/entrypoint.sh
+++ aks-deploy/entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-# Respect AZ_OUTPUT_FORMAT if specified
-[ -n "$AZ_OUTPUT_FORMAT" ] || export AZ_OUTPUT_FORMAT=json
+if [ -z "$AKS_CLUSTER_NAME" ]; then
+    echo "\$AKS_CLUSTER_NAME is not set."
+    exit 1
+fi
 
 if [ -n "$AZURE_SERVICE_PEM" ]; then
   mkdir -p "$HOME/.az"
@@ -11,24 +13,64 @@ if [ -n "$AZURE_SERVICE_PEM" ]; then
   export AZURE_SERVICE_PASSWORD="$HOME/.az/key.pem"
 fi
 
-if [ -n "$AZURE_SERVICE_APP_ID" ] && [ -n "$AZURE_SERVICE_PASSWORD" ] && [ -n "$AZURE_SERVICE_TENANT" ]; then
-  az login --service-principal --username "$AZURE_SERVICE_APP_ID" --password "$AZURE_SERVICE_PASSWORD" --tenant "$AZURE_SERVICE_TENANT"
+if [ -z "$RESOURCE_GROUP" ]; then
+  AMBIGUOUS=$(az resource list --name $AKS_CLUSTER_NAME --resource-type Microsoft.ContainerService/managedClusters --query "[1].resourceGroup")
+  if [ -n "$AMBIGUOUS" ]; then
+    echo "Provided AKS cluster name is ambiguous, provide \$RESOURCE_GROUP to identify the cluster correctly"
+    exit 1
+  else
+    RESOURCE_GROUP=$(az resource list --name $AKS_CLUSTER_NAME --resource-type Microsoft.ContainerService/managedClusters --query "[0].resourceGroup" -o tsv)
+    
+    if [ -z "$RESOURCE_GROUP" ]; then
+      echo "Ensure the AKS cluster: '${AKS_CLUSTER_NAME}' exists."
+      exit 1
+    fi
+    
+    echo "Recognized RG name: $RESOURCE_GROUP"
+  fi
 fi
 
-az aks get-credentials --name "$AKS_CLUSTER_NAME" --resource-group "$RESOURCE_GROUP"
+if [ -z "$KUBECONFIG" ]; then
+  az aks get-credentials --name $AKS_CLUSTER_NAME --resource-group $RESOURCE_GROUP
+  INGRESS_ROUTING_ZONE=$(az aks show -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --query "addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName")
+fi
 
 if [ -n "$DOCKER_REGISTRY_URL" ] && [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
   echo "Adding docker secrets"
-  kubectl create secret docker-registry dockerPullSecret --docker-server=$DOCKER_REGISTRY_URL --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD
+  if [ -n "$DOCKER_EMAIL" ]; then
+    kubectl create secret docker-registry docker-pull-secret --docker-server=$DOCKER_REGISTRY_URL --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL --dry-run -o json | kubectl apply -f -
+  else
+    kubectl create secret docker-registry docker-pull-secret --docker-server=$DOCKER_REGISTRY_URL --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=default@example.com --dry-run -o json | kubectl apply -f -
+  fi
 fi
 
 helm init
 
-if [ -n "$HELM_CHART_PATH" ]; then
-  helm upgrade --install --force $* aksdeploy $HELM_CHART_PATH   
-else
-  echo "Creating a helm chart"
-  helm create aksdeploy
-  HELM_CHART_PATH=./aksdeploy
-  helm upgrade --install --force --set image.repository=$IMAGE_NAME --set image.tag=latest --set service.type=LoadBalancer aksdeploy $HELM_CHART_PATH 
+DEFAULT_ARGS=""
+
+if [ -z "$HELM_RELEASE_NAME" ]; then
+  HELM_RELEASE_NAME=aks-deploy
 fi
+
+if [ -z "$HELM_CHART_PATH" ]; then
+  echo "Using a default helm chart"
+
+  if [ -z "$CONTAINER_IMAGE_NAME " ]; then
+      echo "\$CONTAINER_IMAGE_NAME  is not set."
+      exit 1
+  fi
+
+  HELM_CHART_PATH=/default-chart
+  
+  if [ -n "$CONTAINER_IMAGE_TAG " ]; then
+    CONTAINER_IMAGE_NAME=$CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_TAG 
+  fi
+  
+  DEFAULT_ARGS="--set image.repository=$CONTAINER_IMAGE_NAME"
+
+  if [ -n "${INGRESS_ROUTING_ZONE}" ]; then
+    DEFAULT_ARGS="--set image.repository=$CONTAINER_IMAGE_NAME --set ingress.enabled=true --set ingress.hostname=${HELM_RELEASE_NAME}.${INGRESS_ROUTING_ZONE}"
+  fi
+fi
+
+helm upgrade --install --force $DEFAULT_ARGS $* $HELM_RELEASE_NAME $HELM_CHART_PATH
