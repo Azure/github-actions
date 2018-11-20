f1e7eab7316fa379461d81fc66f928a3fa16e825
Vincent A (vinca) <vinca@microsoft.com>
2018-11-20 11:50:48 +0530

    Container App - Address Review comments (#13)
    * rename variable as per spec
    
    * res updaed
    
    * set required variable in app

--- web-app-container/entrypoint.sh
+++ web-app-container/entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [[ -z $WEB_APP_NAME ]];
+if [[ -z $AZURE_APP_NAME ]];
 then 
-    echo "Required Web App Name. Provide value in WEB_APP_NAME variable" >&2
+    echo "Required Web App Name. Provide value in AZURE_APP_NAME variable" >&2
     exit 1
 fi
 
-echo "Web App Name: ${WEB_APP_NAME}"
+echo "Web App Name: ${AZURE_APP_NAME}"
 
 if [[ -z $CONTAINER_IMAGE_NAME ]];
 then
@@ -16,11 +16,11 @@ then
     exit 1
 fi
 
-RESOURCE_GROUP_NAME=`az resource list -n "${WEB_APP_NAME}" --resource-type "Microsoft.Web/Sites" --query '[0].resourceGroup' | xargs`
+RESOURCE_GROUP_NAME=`az resource list -n "${AZURE_APP_NAME}" --resource-type "Microsoft.Web/Sites" --query '[0].resourceGroup' | xargs`
 
 if [[ -z $RESOURCE_GROUP_NAME ]];
 then
-    echo "Web App '${WEB_APP_NAME}' should exist before deployment." >&2
+    echo "Web App '${AZURE_APP_NAME}' should exist before deployment." >&2
     exit 1
 fi
 
@@ -38,16 +38,16 @@ then
     AZCLI_ARGUMENT="${AZCLI_ARGUMENT} --docker-registry-server-user ${DOCKER_USERNAME} --docker-registry-server-password ${DOCKER_PASSWORD}"
 fi
 
-if [[ ! -z $DOCKER_REGISTRY_SERVER_URL ]]
+if [[ ! -z $DOCKER_REGISTRY_URL ]]
 then
-    AZCLI_ARGUMENT="${AZCLI_ARGUMENT} --docker-registry-server-url ${DOCKER_REGISTRY_SERVER_URL}"
+    AZCLI_ARGUMENT="${AZCLI_ARGUMENT} --docker-registry-server-url ${DOCKER_REGISTRY_URL}"
 fi
 
 echo "Initiating Container deployment..."
 
-az webapp config container set -n "${WEB_APP_NAME}" -g "${RESOURCE_GROUP_NAME}" $AZCLI_ARGUMENT
+az webapp config container set -n "${AZURE_APP_NAME}" -g "${RESOURCE_GROUP_NAME}" $AZCLI_ARGUMENT
 
-echo "Configured image details to Azure App Service"
+echo "Configured image details to Azure Web App"
 
-DESTINATION_URL=`az webapp deployment list-publishing-profiles -n ${WEB_APP_NAME} -g ${RESOURCE_GROUP_NAME} --query '[0].destinationAppUrl' -o tsv`
-echo "App Service Application URL: ${DESTINATION_URL}"
+DESTINATION_URL=`az webapp deployment list-publishing-profiles -n ${AZURE_APP_NAME} -g ${RESOURCE_GROUP_NAME} --query '[0].destinationAppUrl' -o tsv`
+echo "Web App Application URL: ${DESTINATION_URL}"
