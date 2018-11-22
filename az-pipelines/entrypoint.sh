91ad5cbfa9116fcbf5223a4c6636d947be2a9af6
nibansal <nibansal@microsoft.com>
2018-11-22 22:01:14 +0530

    Action updated

--- az-pipelines/entrypoint.sh
+++ az-pipelines/entrypoint.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
-set -e
+set -e 
 
-if [ -z "$AZURE_PIPELINE_ORGANIZATION" ]; then
-    echo "\$AZURE_PIPELINE_ORGANIZATION is not set."
+if [ -z "$AZURE_PIPELINE_ORGANIZATION" ]; 
+then
+    echo "\$AZURE_PIPELINE_ORGANIZATION is not set." >&2
     exit 1
 fi
 
-if [ -z "$AZURE_PIPELINE_PROJECT" ]; then
-    echo "\$AZURE_PIPELINE_PROJECT is not set."
+if [ -z "$AZURE_PIPELINE_PROJECT" ]; 
+then
+    echo "\$AZURE_PIPELINE_PROJECT is not set." >&2
     exit 1
 fi
 
-if [ -z "$AZURE_PIPELINE_TOKEN" ]; then
-    echo "\$AZURE_PIPELINE_TOKEN is not set."
+if [ -z "$AZURE_PIPELINE_TOKEN" ]; 
+then
+    echo "\$AZURE_PIPELINE_TOKEN is not set." >&2
     exit 1
 fi
 
-if [ -z "$AZURE_PIPELINE_NAME" ]; then
-    echo "\$AZURE_PIPELINE_NAME is not set."
+if [ -z "$AZURE_PIPELINE_NAME" ]; 
+then
+    echo "\$AZURE_PIPELINE_NAME is not set." >&2
     exit 1
 fi
 
@@ -27,6 +31,38 @@ AZDEVOPS_URL="https://dev.azure.com/${AZURE_PIPELINE_ORGANIZATION}/"
 vsts configure --defaults instance=${AZDEVOPS_URL} project=${AZURE_PIPELINE_PROJECT}
     
 vsts login --token ${AZURE_PIPELINE_TOKEN}
-    
-echo "Queueing Azure pipeline: ${AZURE_PIPELINE_NAME}"
-vsts build queue --definition-name ${AZURE_PIPELINE_NAME}
+  
+PIPELINES=$( vsts build definition list --name ${AZURE_PIPELINE_NAME} )
+COUNT=$( echo ${PIPELINES} | jq length )
+
+if [ $COUNT -eq 0 ]; 
+then
+   echo "No build definition found with name: ${AZURE_PIPELINE_NAME}". >&2
+   exit 1;
+fi
+
+if [ $COUNT -gt 1 ]; 
+then
+    echo "Multple build definitions were found with name: ${AZURE_PIPELINE_NAME}. Pass unique build definition name and try again." >&2
+    exit 1;
+fi
+
+BUILD_DEFINITION_ID=$( echo ${PIPELINES} | jq .[0].id -r )
+BUILD_DEFINITION=$( vsts build definition show --id ${BUILD_DEFINITION_ID} )
+REPOSITORY_NAME=$( echo ${BUILD_DEFINITION} | jq .repository.name -r )
+REPOSITORY_TYPE=$( echo ${BUILD_DEFINITION} | jq .repository.type -r )
+
+if [ "$REPOSITORY_NAME" = "$GITHUB_REPOSITORY" ] && ["$REPOSITORY_TYPE" = "GitHub" ]; 
+then
+     vsts build queue --definition-name ${AZURE_PIPELINE_NAME} --branch ${GITHUB_REF} --commit-id ${GITHUB_SHA}
+else
+    BUILD_OUTPUT=$( vsts build queue --definition-name ${AZURE_PIPELINE_NAME} )
+    echo "${BUILD_OUTPUT}"
+	ERROR="error"
+    MESSAGE=$( echo ${BUILD_OUTPUT} | jq -r ".validationResults[] | select(.result==\"$ERROR\") | .message" ) ;
+	if [ -n "$MESSAGE" ];
+	then
+	         echo "Failed to queue build. Reason: ${MESSAGE}" >&2
+             exit 1;
+	fi
+fi
--- az-release-pipelines/entrypoint.sh
+++ az-release-pipelines/entrypoint.sh
@@ -1,23 +1,26 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
-if [ -z "$AZURE_PIPELINE_ORGANIZATION" ]; then
+if [ -z "$AZURE_PIPELINE_ORGANIZATION" ]; 
+then
     echo "\$AZURE_PIPELINE_ORGANIZATION is not set."
     exit 1
 fi
 
-if [ -z "$AZURE_PIPELINE_PROJECT" ]; then
+if [-z "$AZURE_PIPELINE_PROJECT" ];
+then
     echo "\$AZURE_PIPELINE_PROJECT is not set."
     exit 1
 fi
 
-if [ -z "$AZURE_PIPELINE_TOKEN" ]; then
+if [-z "$AZURE_PIPELINE_TOKEN" ]; 
+then
     echo "\$AZURE_PIPELINE_TOKEN is not set."
     exit 1
 fi
 
-if [ -z "$AZURE_PIPELINE_NAME" ]; then
+if [ -z "$AZURE_PIPELINE_NAME" ]; 
+then
     echo "\$AZURE_PIPELINE_NAME is not set."
     exit 1
 fi
@@ -27,6 +30,41 @@ AZDEVOPS_URL="https://dev.azure.com/${AZURE_PIPELINE_ORGANIZATION}/"
 vsts configure --defaults instance=${AZDEVOPS_URL} project=${AZURE_PIPELINE_PROJECT}
     
 vsts login --token ${AZURE_PIPELINE_TOKEN}
+
+PIPELINES=$( vsts release definition list --name ${AZURE_PIPELINE_NAME} )
+PIPELINE_COUNT=$( echo ${PIPELINES} | jq length )
+
+if [ $PIPELINE_COUNT -eq 0 ]
+then
+   echo "No release definition found with name: ${AZURE_PIPELINE_NAME}". >&2
+   exit 1;
+fi
+
+COUNT=0
+echo ${PIPELINES} | jq .[]?.name -r | while read PIPELINE ; 
+do
+   if [ "$PIPELINE" =  "$AZURE_PIPELINE_NAME" ]; 
+   then
+       COUNT=`expr $COUNT + 1`
+	   if [ $COUNT -gt 1 ]; 
+       then
+           echo "Multple release definitions were found with name: ${AZURE_PIPELINE_NAME}. Pass unique release definition name and try again." >&2
+           exit 1;
+        fi
+   fi
+done
+
+RELEASE_DEFINITION=$( vsts release definition show --name ${AZURE_PIPELINE_NAME} )
+echo $RELEASE_DEFINITION
+TYPE="GitHub"
+ALIAS=$( echo ${RELEASE_DEFINITION} | jq -r ".artifacts[] | select((.type==\"$TYPE\") and .definitionReference.definition.name==\"$GITHUB_REPOSITORY\") | .alias" )
+
+if [ -n "$ALIAS" ]; 
+then
+    echo "Triggering Azure release pipeline for : ${AZURE_PIPELINE_NAME} for commitId: ${GITHUB_SHA}."
+    vsts release create --definition-name ${AZURE_PIPELINE_NAME} --artifact-metadata-list "$ALIAS"="$GITHUB_SHA"
+	exit 0
+fi
     
 echo "Triggering Azure release pipeline: ${AZURE_PIPELINE_NAME}"
 vsts release create --definition-name ${AZURE_PIPELINE_NAME}
