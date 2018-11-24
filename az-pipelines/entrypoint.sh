2a53e04aefe016649332cecb52128a05e7d77a99
nibansal <nibansal@microsoft.com>
2018-11-24 13:53:15 +0530

    Exception handling added in Build action

--- az-pipelines/entrypoint.sh
+++ az-pipelines/entrypoint.sh
@@ -32,7 +32,7 @@ vsts configure --defaults instance=${AZDEVOPS_URL} project=${AZURE_PIPELINE_PROJ
     
 vsts login --token ${AZURE_PIPELINE_TOKEN}
     
-PIPELINES=$( vsts build definition list --name ${AZURE_PIPELINE_NAME} )
+PIPELINES=$( vsts build definition list --name ${AZURE_PIPELINE_NAME} --output json )
 
 echo ${PIPELINES} | jq -e . > /dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -55,7 +55,7 @@ then
 fi
 
 BUILD_DEFINITION_ID=$( echo ${PIPELINES} | jq -r ".[0]?.id //empty" )
-BUILD_DEFINITION=$( vsts build definition show --id ${BUILD_DEFINITION_ID} )
+BUILD_DEFINITION=$( vsts build definition show --id ${BUILD_DEFINITION_ID} --output json )
 
 echo ${BUILD_DEFINITION} | jq -e . > /dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -63,27 +63,38 @@ if [ $? -ne 0 ]; then
     exit 1;
 fi 
 
-REPOSITORY_NAME=$( echo ${BUILD_DEFINITION} | jq -r ".repository.name //empty" )
-REPOSITORY_TYPE=$( echo ${BUILD_DEFINITION} | jq  -r ".repository.type  //empty" )
+REPOSITORY_NAME=$( echo ${BUILD_DEFINITION} | jq -r ".repository?.name? //empty" )
+REPOSITORY_TYPE=$( echo ${BUILD_DEFINITION} | jq  -r ".repository?.type?  //empty" )
 
 if [ -n "$REPOSITORY_NAME" ] && [ -n "$REPOSITORY_TYPE" ] && [ "$REPOSITORY_NAME" = "$GITHUB_REPOSITORY" ] && [ "$REPOSITORY_TYPE" = "GitHub" ]; 
 then
-    BUILD_OUTPUT=$( vsts build queue --definition-name ${AZURE_PIPELINE_NAME} --branch ${GITHUB_REF} --commit-id ${GITHUB_SHA} )
+    BUILD_OUTPUT=$( vsts build queue --definition-name ${AZURE_PIPELINE_NAME} --branch ${GITHUB_REF} --commit-id ${GITHUB_SHA} --output json )
 else
-    BUILD_OUTPUT=$( vsts build queue --definition-name ${AZURE_PIPELINE_NAME} )
+    BUILD_OUTPUT=$( vsts build queue --definition-name ${AZURE_PIPELINE_NAME} --output json )
+fi
+
+if [ -z "$BUILD_OUTPUT" ];
+then
+    echo "Failed to queue build."
+	exit 1;
 fi
 
 echo ${BUILD_OUTPUT} | jq -e . > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Failed to queue pipeline. Error: ${BUILD_OUTPUT}"
     exit 1;
-fi 
+fi
 
 echo "${BUILD_OUTPUT}"
 ERROR="error"
-MESSAGE=$( echo ${BUILD_OUTPUT} | jq -r ".validationResults[]? | select(.result==\"$ERROR\") | .message //empty" ) ;
-if [ -n "$MESSAGE" ];
+ERROR_COUNT=$( echo ${BUILD_OUTPUT} | jq -r ".validationResults[]? | select(.result?==\"$ERROR\") | .result" | wc -l )
+if [ $ERROR_COUNT -gt 0 ];
 then
-    echo "Failed to queue pipeline. Error: ${MESSAGE}" >&2
+    echo "Failed to queue pipeline." >&2
+    MESSAGE=$(echo ${BUILD_OUTPUT} | jq -r ".validationResults[]?| select(.result?==\"$ERROR\")| .message //empty" | tr '\n' ','| sed  's/.$/./' )
+    if [ -n "$MESSAGE" ];
+    then
+        echo "Validation result contains error. Error: ${MESSAGE}" >&2
+    fi
     exit 1;
 fi
