3164c021818c067824263eb39670394267a2b93f
Deepak Sattiraju <desattir@microsoft.com>
2018-11-27 10:58:26 +0530

    [aks-deploy] Adding support for overriding container port (#23)

--- aks-deploy/entrypoint.sh
+++ aks-deploy/entrypoint.sh
@@ -10,20 +10,20 @@ fi
 if [ -z "$KUBECONFIG" ]; then
 
   if [ -z "$AKS_CLUSTER_NAME" ]; then
-      echo "\$AKS_CLUSTER_NAME is not set."
+      echo "\$AKS_CLUSTER_NAME is not set." >&2
       exit 1
   fi
 
   if [ -z "$RESOURCE_GROUP" ]; then
     AMBIGUOUS=$(az resource list --name $AKS_CLUSTER_NAME --resource-type Microsoft.ContainerService/managedClusters --query "[1].resourceGroup")
     if [ -n "$AMBIGUOUS" ]; then
-      echo "Provided AKS cluster name is ambiguous, provide \$RESOURCE_GROUP to identify the cluster correctly"
+      echo "Provided AKS cluster name is ambiguous, provide \$RESOURCE_GROUP to identify the cluster correctly" >&2
       exit 1
     else
       RESOURCE_GROUP=$(az resource list --name $AKS_CLUSTER_NAME --resource-type Microsoft.ContainerService/managedClusters --query "[0].resourceGroup" -o tsv)
       
       if [ -z "$RESOURCE_GROUP" ]; then
-        echo "Ensure the AKS cluster: '${AKS_CLUSTER_NAME}' exists."
+        echo "Ensure the AKS cluster: '${AKS_CLUSTER_NAME}' exists." >&2
         exit 1
       fi
       
@@ -57,7 +57,7 @@ if [ -z "$HELM_CHART_PATH" ]; then
   echo "Using a default helm chart"
 
   if [ -z "$CONTAINER_IMAGE_NAME" ]; then
-      echo "\$CONTAINER_IMAGE_NAME  is not set."
+      echo "\$CONTAINER_IMAGE_NAME  is not set." >&2
       exit 1
   fi
 
