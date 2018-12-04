4743b1aea96bfaf155b621ea8bd64d857d9bb2e8
Vincent A (vinca) <vinca@microsoft.com>
2018-12-04 07:54:57 +0530

    Update entrypoint.sh

--- web-app-deploy/entrypoint.sh
+++ web-app-deploy/entrypoint.sh
@@ -41,7 +41,7 @@ then
     NEW_PACKAGE_LOCATION="${GITHUB_WORKSPACE}/package_$RANDOM.zip"
     echo "Compressing Package '${AZURE_APP_PACKAGE_LOCATION}' to '$NEW_PACKAGE_LOCATION'"
     cd $AZURE_APP_PACKAGE_LOCATION
-    zip -b /tmp -r "$NEW_PACKAGE_LOCATION" * >/dev/null
+    zip -r "$NEW_PACKAGE_LOCATION" * >/dev/null
     echo  $?
     AZURE_APP_PACKAGE_LOCATION="$NEW_PACKAGE_LOCATION"
     cd "$GITHUB_WORKSPACE"
