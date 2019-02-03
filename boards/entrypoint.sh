3e489b9a865d60104dc4828d5df67c03ec0422e3
Edward Thomson <ethomson@edwardthomson.com>
2019-02-03 15:33:08 +0000

    boards: parse title and description from the event
    GitHub Actions writes the event information to a file specified by
    `GITHUB_EVENT_PATH`.  Parse that file (using `jq`) for the issue title
    and issue body that can be provided to Azure Boards.

--- boards/entrypoint.sh
+++ boards/entrypoint.sh
@@ -26,24 +26,13 @@ then
     exit 1
 fi
 
-if [ -z "$AZURE_BOARDS_TITLE" ]; 
-then
-    echo "AZURE_BOARDS_TITLE is not set." >&2
-    exit 1
-fi
-
-if [ -z "$AZURE_BOARDS_DESCRIPTION" ]; 
-then
-    echo "AZURE_BOARDS_DESCRIPTION is not set." >&2
-    exit 1
-fi
-    
 AZDEVOPS_URL="https://dev.azure.com/${AZURE_BOARDS_ORGANIZATION}/"
 vsts configure --defaults instance=${AZDEVOPS_URL} project="${AZURE_BOARDS_PROJECT}"
-    
-vsts login --token ${AZURE_BOARDS_TOKEN}
+
+vsts login --token "${AZURE_BOARDS_TOKEN}"
+
+AZURE_BOARDS_TITLE=$(jq --raw-output .issue.title "$GITHUB_EVENT_PATH")
+AZURE_BOARDS_DESCRIPTION=$(jq --raw-output .issue.body "$GITHUB_EVENT_PATH")
 
 BOARDS_CREATE=$( vsts work item create --type "${AZURE_BOARDS_TYPE}" --title "${AZURE_BOARDS_TITLE}" --description "${AZURE_BOARDS_DESCRIPTION}" -f 80="FromGitHub" --output json )
-
-
 
