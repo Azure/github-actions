5d946790730cd8a9772b53c0220f85841e64dd32
Edward Thomson <ethomson@edwardthomson.com>
2019-02-03 20:57:39 +0000

    boards: add issue comments to work item discussion

--- boards/entrypoint.sh
+++ boards/entrypoint.sh
@@ -22,6 +22,10 @@ if [ -z "$GITHUB_EVENT_PATH" ]; then
     exit 1
 fi
 
+function work_items_for_issue {
+    vsts work item query --wiql "SELECT ID FROM workitems WHERE [System.Tags] CONTAINS 'GitHub' AND [System.Tags] CONTAINS 'Issue ${GITHUB_ISSUE_NUMBER}'" | jq '.[].id' | xargs
+}
+
 AZURE_BOARDS_TYPE="${AZURE_BOARDS_TYPE:-Feature}"
 AZURE_BOARDS_CLOSED_STATE="${AZURE_BOARDS_CLOSED_STATE:-Done}"
 AZURE_BOARDS_REOPENED_STATE="${AZURE_BOARDS_REOPENED_STATE:-New}"
@@ -31,13 +35,18 @@ vsts configure --defaults instance="${AZURE_DEVOPS_URL}" project="${AZURE_BOARDS
 
 vsts login --token "${AZURE_BOARDS_TOKEN}"
 
+GITHUB_EVENT=$(jq --raw-output 'if .comment != null then "comment" else empty end' "$GITHUB_EVENT_PATH")
+GITHUB_EVENT=${GITHUB_EVENT:-$(jq --raw-output 'if .issue != null then "issue" else empty end' "$GITHUB_EVENT_PATH")}
+
 GITHUB_ACTION=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
 GITHUB_ISSUE_NUMBER=$(jq --raw-output .issue.number "$GITHUB_EVENT_PATH")
 AZURE_BOARDS_TITLE=$(jq --raw-output .issue.title "$GITHUB_EVENT_PATH")
 AZURE_BOARDS_DESCRIPTION=$(jq --raw-output .issue.body "$GITHUB_EVENT_PATH")
 
-case "$GITHUB_ACTION" in
-"opened")
+TRIGGER="${GITHUB_EVENT}/${GITHUB_ACTION}"
+
+case "$TRIGGER" in
+"issue/opened")
     echo "Creating work item..."
     RESULTS=$(vsts work item create --type "${AZURE_BOARDS_TYPE}" \
         --title "${AZURE_BOARDS_TITLE}" \
@@ -48,13 +57,14 @@ case "$GITHUB_ACTION" in
 
     echo "Created work item #${AZURE_BOARDS_ID}"
     ;;
-"reopened"|"closed")
+
+"issue/reopened"|"issue/closed")
     [[ "$GITHUB_ACTION" = "reopened" ]] && \
         NEW_STATE="$AZURE_BOARDS_REOPENED_STATE" || \
         NEW_STATE="$AZURE_BOARDS_CLOSED_STATE"
 
     echo "Looking for work items with tag 'Issue ${GITHUB_ISSUE_NUMBER}'..."
-    IDS=$(vsts work item query --wiql "SELECT ID FROM workitems WHERE [System.Tags] CONTAINS 'GitHub' AND [System.Tags] CONTAINS 'Issue ${GITHUB_ISSUE_NUMBER}'" | jq '.[].id' | xargs)
+    IDS=$(work_items_for_issue)
 
     for ID in ${IDS}; do
         echo "Setting work item ${ID} to state ${NEW_STATE}..."
@@ -62,6 +72,19 @@ case "$GITHUB_ACTION" in
 
         RESULT_STATE=$(echo "${RESULTS}" | jq --raw-output '.fields["System.State"]')
         echo "Work item ${ID} is now ${RESULT_STATE}"
+    done
+    ;;
+
+"comment/created")
+    echo "Looking for work items with tag 'Issue ${GITHUB_ISSUE_NUMBER}'..."
+    IDS=$(work_items_for_issue)
+
+    for ID in ${IDS}; do
+        HEADER="Comment from @$(jq --raw-output .comment.user.login "$GITHUB_EVENT_PATH"): "
+        BODY=$(jq --raw-output .comment.body "$GITHUB_EVENT_PATH")
+
+        echo "Adding comment to work item ${ID}..."
+        RESULTS=$(vsts work item update --id "$ID" --discussion "${HEADER}${BODY}")
     done
     ;;
 esac
