06ea17ab7365a3dd8cc5acc16009329dc833093d
Deepak Sattiraju <desattir@microsoft.com>
2019-02-05 11:26:57 +0530

    aks/entrypoint.sh

--- aks/entrypoint.sh
+++ aks/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 export AZURE_HTTP_USER_AGENT="GITHUBACTIONS_${GITHUB_ACTION_NAME}_${GITHUB_REPOSITORY}"
@@ -16,12 +16,12 @@ if [ -z "$KUBECONFIG" ]; then
   fi
 
   if [ -z "$RESOURCE_GROUP" ]; then
-    AMBIGUOUS=$(az resource list --name $AKS_CLUSTER_NAME --resource-type Microsoft.ContainerService/managedClusters --query "[1].resourceGroup")
+    AMBIGUOUS=$(az resource list --name "$AKS_CLUSTER_NAME" --resource-type Microsoft.ContainerService/managedClusters --query "[1].resourceGroup")
     if [ -n "$AMBIGUOUS" ]; then
       echo "Provided AKS cluster name is ambiguous, provide \$RESOURCE_GROUP to identify the cluster correctly" >&2
       exit 1
     else
-      RESOURCE_GROUP=$(az resource list --name $AKS_CLUSTER_NAME --resource-type Microsoft.ContainerService/managedClusters --query "[0].resourceGroup" -o tsv)
+      RESOURCE_GROUP=$(az resource list --name "$AKS_CLUSTER_NAME" --resource-type Microsoft.ContainerService/managedClusters --query "[0].resourceGroup" -o tsv)
       
       if [ -z "$RESOURCE_GROUP" ]; then
         echo "Ensure the AKS cluster: '${AKS_CLUSTER_NAME}' exists." >&2
@@ -32,17 +32,17 @@ if [ -z "$KUBECONFIG" ]; then
     fi
   fi
 
-  az aks get-credentials --name $AKS_CLUSTER_NAME --resource-group $RESOURCE_GROUP
-  INGRESS_ROUTING_ZONE=$(az aks show -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --query "addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName" -o tsv)
+  az aks get-credentials --name "$AKS_CLUSTER_NAME" --resource-group "$RESOURCE_GROUP"
+  INGRESS_ROUTING_ZONE=$(az aks show -n "$AKS_CLUSTER_NAME" -g "$RESOURCE_GROUP" --query "addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName" -o tsv)
 
 fi
 
 if [ -n "$DOCKER_REGISTRY_URL" ] && [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
   echo "Adding docker secrets"
   if [ -n "$DOCKER_EMAIL" ]; then
-    kubectl create secret docker-registry docker-pull-secret --docker-server=$DOCKER_REGISTRY_URL --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL --dry-run -o json | kubectl apply -f -
+    kubectl create secret docker-registry docker-pull-secret --docker-server="$DOCKER_REGISTRY_URL" --docker-username="$DOCKER_USERNAME" --docker-password="$DOCKER_PASSWORD" --docker-email="$DOCKER_EMAIL" --dry-run -o json | kubectl apply -f -
   else
-    kubectl create secret docker-registry docker-pull-secret --docker-server=$DOCKER_REGISTRY_URL --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=default@example.com --dry-run -o json | kubectl apply -f -
+    kubectl create secret docker-registry docker-pull-secret --docker-server="$DOCKER_REGISTRY_URL" --docker-username="$DOCKER_USERNAME" --docker-password="$DOCKER_PASSWORD" --docker-email=default@example.com --dry-run -o json | kubectl apply -f -
   fi
 fi
 
@@ -68,11 +68,15 @@ if [ -z "$HELM_CHART_PATH" ]; then
     CONTAINER_IMAGE_NAME=$CONTAINER_IMAGE_NAME:$CONTAINER_IMAGE_TAG 
   fi
   
-  DEFAULT_ARGS="--set image.repository=$CONTAINER_IMAGE_NAME"
+  DEFAULT_ARGS[0]="image.repository=$CONTAINER_IMAGE_NAME"
 
   if [ -n "${INGRESS_ROUTING_ZONE}" ]; then
-    DEFAULT_ARGS="--set image.repository=$CONTAINER_IMAGE_NAME --set ingress.enabled=true --set ingress.hostname=${HELM_RELEASE_NAME}.${INGRESS_ROUTING_ZONE}"
+    DEFAULT_ARGS[1]="ingress.enabled=true"
+    DEFAULT_ARGS[2]="ingress.hostname=${HELM_RELEASE_NAME}.${INGRESS_ROUTING_ZONE}"
   fi
-fi
 
-helm upgrade --install --force $DEFAULT_ARGS $* $HELM_RELEASE_NAME $HELM_CHART_PATH
+  helm upgrade --install --force --set "$(IFS=,; echo "${DEFAULT_ARGS[*]}")" "$@" $HELM_RELEASE_NAME $HELM_CHART_PATH
+
+else 
+  helm upgrade --install --force "$@" $HELM_RELEASE_NAME $HELM_CHART_PATH
+fi
