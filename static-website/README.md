f49a40308fa363b1fd053648f9fb4ee451ff4f99
Guy Waldman <guywald1@gmail.com>
2019-03-21 13:28:27 +0200

    Add action for deploying a static website to Azure Storage

---
+++ static-website/README.md
@@ -0,0 +1,30 @@
+# GitHub Action for deploying to a Static Website in Azure Storage
+
+To log into a Azure, we recommend using the [Azure Login](https://github.com/Azure/github-actions/tree/master/login) Action.
+
+
+## Usage
+
+```
+action "Upload to Static Website in Azure Storage" {
+  needs = ["Azure Login"]
+  uses = "Azure/github-actions/static-website@master"
+  env = {
+    AZURE_STORAGE_ACCOUNT = "<Azure Storage Account>"
+    PUBLIC_FOLDER = "<Public folder from which to deploy>"
+  }
+  secrets = [SAS_TOKEN]
+}
+```
+
+
+## Configuration
+
+### Environment variables
+
+- `AZURE_STORAGE_ACCOUNT` – **Required** 
+- `PUBLIC_FOLDER` - *Optional* (defaults to `public`)
+
+### Secrets
+
+- `SAS_TOKEN`
---
+++ static-website/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+export AZURE_HTTP_USER_AGENT="GITHUBACTIONS_${GITHUB_ACTION_NAME}_${GITHUB_REPOSITORY}"
+
+LINUX_APP_SUBSTRING="linux"
+DEFAULT_PUBLIC_FOLDER="public"
+
+if [[ -z $AZURE_STORAGE_ACCOUNT ]];
+then 
+    echo "A Storage Account Name is required. Please provide it in the AZURE_STORAGE_ACCOUNT environment variable" >&2
+    exit 1
+fi
+
+if [[ -z $SAS_TOKEN ]];
+then 
+    echo "A SAS Token secret is required. Please provide it in the SAS_TOKEN secret" >&2
+    exit 1
+fi
+
+if [[ -z $PUBLIC_FOLDER ]];
+then 
+    echo "No public folder provided in the PUBLIC_FOLDER environment variable. Defaulting to \`/${DEFAULT_PUBLIC_FOLDER}\`" >&2
+    PUBLIC_FOLDER=${DEFAULT_PUBLIC_FOLDER}
+fi
+
+echo "Emptying ${AZURE_STORAGE_ACCOUNT}/\$web"
+
+az storage blob delete-batch -s "\$web"
+
+echo "Successfully emptied ${AZURE_STORAGE_ACCOUNT}/\$web"
+
+echo "Uploading \`/${PUBLIC_FOLDER}\` to ${AZURE_STORAGE_ACCOUNT}/\$web"
+
+az storage blob upload-batch --no-progress -d "\$web" -s ${PUBLIC_FOLDER}
+
+echo "Successfully uploaded \`/${PUBLIC_FOLDER}\` to ${AZURE_STORAGE_ACCOUNT}/\$web"
